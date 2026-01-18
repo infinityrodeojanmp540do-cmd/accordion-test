4fd7e8e992e1b1f14fca9171ee1c9e0f2454438b
infinityrodeojanmp540do-cmd <infinity.rodeojanmp540do@gmail.com>
2026-01-18 22:29:40 +0900

    docs/accordion#6

--- js/accordion.js
+++ js/accordion.js
@@ -1,17 +1,4 @@
+document.querySelectorAll('.js-accordion').forEach((item) => {
+  const content = item.querySelector('.js-accordion-content');
 
-
-document.querySelectorAll('.js-accordion').forEach((accordion, accIndex) => {
-  accordion.querySelectorAll('.js-accordion-item').forEach((item, index) => {
-    const summary = item.querySelector('.js-accordion-trigger');
-    const content = item.querySelector('.js-accordion-content');
-
-    const contentId = `accordion-${accIndex}-${index}`;
-    content.id = contentId;
-
-    summary.setAttribute('aria-controls', contentId);
-    summary.setAttribute(
-      'aria-expanded',
-      item.hasAttribute('open')
-    );
-  });
 });
